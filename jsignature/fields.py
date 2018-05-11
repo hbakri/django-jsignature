49513b7a2d3471be1c6a99f520f4055c57953699
Borovikov <alximick@gmail.com>
2018-05-11 15:32:35 +0700

    Remove the context parameter from JSignatureField.from_db_value()

--- jsignature/fields.py
+++ jsignature/fields.py
@@ -32,7 +32,7 @@ class JSignatureField(models.Field):
         except ValueError:
             raise ValidationError('Invalid JSON format.')
 
-    def from_db_value(self, value, expression, connection, context):
+    def from_db_value(self, value, expression, connection):
         if value in JSIGNATURE_EMPTY_VALUES:
             return None
         try:
